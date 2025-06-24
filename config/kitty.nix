22fccf4699501797557dc027756bf5087d877ee3
Maurice Yang <mauriceyangmy28@gmail.com>
2025-06-24 10:44:24 +0700

    feat: changing fish into zsh

--- config/kitty.nix
+++ config/kitty.nix
@@ -11,7 +11,7 @@
 	opacity=0;
       };
 
-      shellIntegration.enableFishIntegration = true;
+      shellIntegration.enableZshIntegration = true;
       themeFile = "Catppuccin-Macchiato";
       #Also available: Catppuccin-Frappe Catppuccin-Latte Catppuccin-Macchiato Catppuccin-Mocha
       # See all available kitty themes at: https://github.com/kovidgoyal/kitty-themes/blob/46d9dfe230f315a6a0c62f4687f6b3da20fd05e4/themes.json
