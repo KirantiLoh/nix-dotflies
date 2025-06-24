209979e70be949c3b557b4bb457b9e92b0d39922
Maurice Yang <mauriceyangmy28@gmail.com>
2025-06-24 09:18:12 +0700

    feat: added vscode

---
+++ config/vscode.nix
@@ -0,0 +1,25 @@
+{
+programs.vscode = {
+  enable = true;
+  extensions = with pkgs.vscode-extensions; [
+    vscodevim.vim
+
+    vue.vscode-typescript-vue-plugin
+    vue.volar
+    
+    dbaeumer.vscode-eslint
+
+    # Error Related
+    usernamehw.errorlens
+    yoavbls.pretty-ts-errors
+
+    # Theme
+    catppuccin.catppuccin-vsc
+    catppuccin.catppuccin-vsc-icons
+
+    # Misc
+    yzhang.markdown-all-in-one
+  ];
+};
+
+}
