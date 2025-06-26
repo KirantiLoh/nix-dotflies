d3e68cb13c2a16197335b2985f0f75d4ffee4b7d
Maurice Yang <mauriceyangmy28@gmail.com>
2025-06-26 10:40:44 +0700

    feat: adding minimal configuration with statusline and telescope

--- pkgs/nvim/default.nix
+++ pkgs/nvim/default.nix
@@ -6,8 +6,33 @@
     settings = {
       vim.viAlias = false;
       vim.vimAlias = true;
+
+      # Theming
+      vim.theme = {
+	enable = true;
+	name = "catppucin";
+	style = "dark";
+      };
+
+      vim.telescope = {
+	enable = true;
+      };
+
+      vim.statusline.lualine = {
+	enable = true;
+      };
+
       vim.lsp = {
         enable = true;
+      };
+      vim.languages = {
+	enableLSP = true;
+	ts.enable = true;
+	python.enable = true;
+	html.enable = true;
+	go.enable = true;
+	nix.enable = true;
+	sql.enable = true;
       };
     };
   };
